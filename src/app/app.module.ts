2929af83663e53bb80f3baddb5aede9f56a22bb8
Harinivas <harinivas@webneo.in>
2018-03-09 03:12:46 +0530

    Push Notification Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { Push} from '@ionic-native/push';
 @NgModule({
   declarations: [
     MyApp,
@@ -24,6 +24,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    Push,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
